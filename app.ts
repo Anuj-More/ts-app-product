4ef0fe166cf1dbce19f54e5e4d09aa16f9a811f9
Anuj More <work.anujmore@gmail.com>
2026-06-03 00:27:23 +0530

    Add product management features

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ let inventory: Product[] = [
     {id: 2, name: "Bat", price: 200, stock: 7},
 ];
 
-function displayInventory() {
+function displayInventory(): void{
     console.log("\n=====Current Inventory=====");
     inventory.forEach(product => {
         console.log(`ID: ${product.id} | ${product.name} | ${product.price} | ${product.stock}`);
@@ -19,4 +19,29 @@ function displayInventory() {
     console.log("===========================");
 }
 
+function addProduct(newProduct: Product): void{
+    inventory.push(newProduct);
+}
+
+function updateStock(id: number, newStock: number){
+    const product = inventory.find(p => p.id === id);
+
+    if(product){
+        product.stock = newStock;
+    } else {
+        console.log("error: product of that id not found");
+    }
+}
+displayInventory();
+
+// Test Task 1: Add a product
+addProduct({id: 4, name: "Sony WH-1000XM4", price: 24000, stock: 5});
+
+// Test Task 2: Update stock of an existing product (ID: 2)
+updateStock(2, 25);
+
+// Test Error Handling: Try updating a product ID that doesn't exist
+updateStock(99, 10);
+
+// Print the inventory again to see the changes!
 displayInventory();
