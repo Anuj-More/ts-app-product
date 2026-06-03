b67ead97d39634e560eeea9247bc794cfd1aa104
Anuj More <work.anujmore@gmail.com>
2026-06-03 09:09:43 +0530

    create start function turning app into repl of product inventory mgmt

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
 
+import * as readline from 'readline-sync';
+
 interface Product {
     id:  number;
     name: string;
@@ -10,6 +12,43 @@ let inventory: Product[] = [
     {id: 1, name: "Keyboard", price: 500, stock: 4},
     {id: 2, name: "Bat", price: 200, stock: 7},
 ];
+
+function startApp(){
+    let running = true;
+    while(running){
+        console.log("\n====E-commerce manager====");
+        console.log("1. View Inventory");
+        console.log("2. Add Product");
+        console.log("3. Update Product Stock");
+        console.log("4. Exit");
+
+        const choice = readline.question("Choose an option (1-4): ");
+        switch(choice){
+            case "1":
+                displayInventory();
+                break;
+            case "2":
+                const name = readline.question("Enter name: ");
+                const price = parseFloat(readline.question("Enter price: "));
+                const stock = parseInt(readline.question("Enter stock: "));
+
+                if(!name || isNaN(price) || isNaN(stock)){
+                    console.log("invalid inputs");
+                    break;
+                }
+                addProduct(name, price, stock);
+                break;
+            case "3":
+                break;
+            case "4":
+                console.log("Exiting");
+                running = false;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 function displayInventory(): void{
     console.log("\n=====Current Inventory=====");
@@ -43,16 +82,5 @@ function updateStock(id: number, newStock: number){
         console.log("error: product of that id not found");
     }
 }
-displayInventory();
 
-// Test Task 1: Add a product
-addProduct({id: 4, name: "Sony WH-1000XM4", price: 24000, stock: 5});
-
-// Test Task 2: Update stock of an existing product (ID: 2)
-updateStock(2, 25);
-
-// Test Error Handling: Try updating a product ID that doesn't exist
-updateStock(99, 10);
-
-// Print the inventory again to see the changes!
-displayInventory();
+startApp();
